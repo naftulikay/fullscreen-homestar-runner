b5ab66fb7b9adf0d9fde7625900edf246a94ac51
TK Kocheran <rfkrocktk@gmail.com>
2011-08-01 17:24:30 -0700

    Initial commit of the Fullscreen Homestar Runner browser plugin/Greasemonkey script.

---
+++ fullscreen-homestar-runner.user.js
@@ -0,0 +1,61 @@
+// ORIGINAL VERSION:
+// Homestar-Fullon
+// version 0.2
+// 2005-04-26
+// Copyright (c) 2005, T Rice, timgm@bcheck.net
+// Released under the GPL license
+// http://www.gnu.org/copyleft/gpl.html
+// 
+// REVAMPED/REVISITED VERSION:
+// Fullscreen Homestar Runner
+// version 1.1
+// 2011-08-01
+// Released under the GPLv3 license
+// --------------------------------------------------------------------
+//
+// ==UserScript==
+// @name          Fullscreen Homestar Runner
+// @version	      1.1
+// @namespace     http://tkassembled.com/ns/2011/fullscreen-homestar-runner
+// @description	  Make all of homestarrunner's cartoons fill your browser window
+// @include       http://*.homestarrunner.com/*
+// @include       http://homestarrunner.com/*
+// @include		  https://*.homestarrunner.com/*
+// @include		  https://homestarrunner.com/*
+// @run-at		  document-end
+// ==/UserScript==
+
+(function() {
+	function resize() {
+		var objs=document.evaluate("//EMBED", document, null,
+			XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+
+		var o = objs.snapshotItem(0);
+		var bar = objs.snapshotItem(1);
+
+		if(o && o.width && o.height && o.width > 0 && o.height > 0) {
+			var dw = window.innerWidth;
+			var dh = window.innerHeight - (bar && bar.height ? bar.height * 2 : 0);
+			var ar = o.width/o.height;
+      
+			if(dw / ar <= dh) {
+				dh = Math.floor(dw / ar);
+			} else {
+				dw = Math.floor(dh * ar);
+			}
+			
+			/* set embed's size */
+			o.width = dw;
+			o.height = dh;
+		}
+	}
+
+	/* remove margin */
+	document.body.style.margin = "0px";
+
+	/* resize embed when window is resized */
+	window.addEventListener("resize", resize, false);
+
+	/* resize on first load */
+	resize();
+})();
